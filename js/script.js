236d08a2ad7a31e51b1f5298f9e6f05ffba4173d
Harsha2122 <tsingh2214@gmail.com>
2023-11-05 02:28:29 +0530

    ashhads

--- js/script.js
+++ js/script.js
@@ -13,29 +13,6 @@ const scroll = new LocomotiveScroll({
     smooth: true,
 });
 
-const boardingDate = document.getElementById('boarding-date');
-const returningDate = document.getElementById('returning-date');
 
-
-const datepickerOptions = {
-    onSelect: (instance, date) => {
-        instance.el.value = date.toLocaleDateString();
-    }
-};
-
-datepicker(boardingDate, datepickerOptions);
-datepicker(returningDate, datepickerOptions);
-
-function search() {
-    const journeyFrom = document.getElementById('journey-from').value;
-    const journeyTo = document.getElementById('journey-to').value;
-    const boardingDateValue = boardingDate.value;
-    const returningDateValue = returningDate.value;
-
-    console.log('Journey From:', journeyFrom);
-    console.log('Journey To:', journeyTo);
-    console.log('Boarding Date:', boardingDateValue);
-    console.log('Returning Date:', returningDateValue);
-}
 
 
