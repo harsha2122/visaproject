f2eea2635c3dcd9335b038a6f7d3cd4919febcfd
Harsha2122 <tsingh2214@gmail.com>
2023-11-06 15:41:41 +0530

    6nov2

--- js/script.js
+++ js/script.js
@@ -4,30 +4,36 @@ function initLocoScroll() {
         smooth: true
     });
 
+    const nav = document.getElementById("nav");
+    const nav1 = document.getElementById("nav1");
+
+    nav1.style.opacity = "0"; 
+    nav.style.opacity = "1";
+    nav1.style.pointerEvents = "none";
+
     locoScroll.on("scroll", (e) => {
-        const nav = document.getElementById("nav");
         const scrollDistance = e.scroll.y;
 
-        const page1 = document.querySelector(".page1");
+        const page1 = document.querySelector(".vacation");
         const page1Position = page1.offsetTop;
-        const changeColorPoint = page1Position + page1.offsetHeight;
+        const changeColorPoint = page1Position;
 
-        if (scrollDistance > changeColorPoint) {
-
-            gsap.to(nav, {
-                backgroundColor: "#000", 
-                duration: 0.5 
-            });
+        if (scrollDistance >= changeColorPoint) {
+            nav.style.opacity = "0";
+            nav1.style.opacity = "1";
+            nav.style.pointerEvents = "none"; 
+            nav1.style.pointerEvents = "auto"; 
         } else {
-            gsap.to(nav, {
-                backgroundColor: "rgba(0,0,0,0)",
-                duration: 0.5
-            });
+            nav.style.opacity = "1";
+            nav1.style.opacity = "0";
+            nav.style.pointerEvents = "auto"; 
+            nav1.style.pointerEvents = "none";
         }
     });
 }
 
 initLocoScroll();
+
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
