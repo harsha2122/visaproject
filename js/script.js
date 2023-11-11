73fff8fd80f2b66da7d812affc6ad2093e8f9c93
Harsha2122 <tsingh2214@gmail.com>
2023-11-11 23:19:16 +0530

    11nov

--- js/script.js
+++ js/script.js
@@ -5,17 +5,13 @@ function initLocoScroll() {
     });
 
     const nav = document.getElementById("nav");
-    const nav1 = document.getElementById("nav1");
 
     function handleNavigationVisibility() {
         if (window.innerWidth <= 768) {
             nav.style.display = "flex";
-            nav1.style.display = "none"; 
         } else {
- 
-            nav.style.opacity = "1"; 
-            nav1.style.opacity = "0"; 
-            nav1.style.pointerEvents = "none";
+            nav.style.opacity = "1";
+            nav.style.transition = "background-color 0.3s ease-in-out, color 0.3s ease-in-out"; // Add transition property
 
             locoScroll.on("scroll", (e) => {
                 const scrollDistance = e.scroll.y;
@@ -25,20 +21,17 @@ function initLocoScroll() {
                 const changeColorPoint = page1Position;
 
                 if (scrollDistance >= changeColorPoint) {
-                    nav.style.opacity = "0"; 
-                    nav1.style.opacity = "1"; 
-                    nav.style.pointerEvents = "none"; 
-                    nav1.style.pointerEvents = "auto";
+                    // Change background color to white
+                    nav.style.background = "black";
+                    nav.style.color = "black";
                 } else {
-                    nav.style.opacity = "1";
-                    nav1.style.opacity = "0";
-                    nav.style.pointerEvents = "auto"; 
-                    nav1.style.pointerEvents = "none"; 
+                    // Reset to original styles
+                    nav.style.background = "transparent";
+                    nav.style.color = "black";
                 }
             });
         }
     }
-
 
     handleNavigationVisibility();
 
@@ -46,7 +39,6 @@ function initLocoScroll() {
 }
 
 initLocoScroll();
-
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
@@ -77,3 +69,8 @@ var swiper = new Swiper(".mySwiper", {
     });
     });
 
+    function toggleMenu() {
+        var mobileMenu = document.getElementById('mobile-menu');
+        mobileMenu.style.display = (mobileMenu.style.display === 'flex') ? 'none' : 'flex';
+        mobileMenu.classList.toggle('hamburger-open');
+    }
